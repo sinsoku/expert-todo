d2059721b00bc8047b461cfb89cd53cbf17f5ee4
sinsoku <sinsoku.listy@gmail.com>
2018-03-19 17:09:57 +0900

    :sparkles: Send a confirmation email after sign up

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "email_#{n}@example.com" }
     password 'password'
+
+    trait :confirmed do
+      confirmed_at { Time.current }
+    end
   end
 end
--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Sign in", type: :system do
-  let!(:user) { create :user }
+  let!(:user) { create :user, :confirmed }
 
   it "sign in" do
     visit "/"
