9af64b3b449de6f069ec1b9cbab1ad64ea006526
sinsoku <sinsoku.listy@gmail.com>
2018-03-19 17:27:53 +0900

    :sparkles: Configure ActionMailer settings for SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: "sinsoku-expert-todo.herokuapp.com" }
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "herokuapp.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
