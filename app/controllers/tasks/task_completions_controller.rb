c7750adbb02fd083f6bd4d7c70cde799e29e7b96
sinsoku <sinsoku.listy@gmail.com>
2018-03-20 16:01:17 +0900

    :recycle: Refactor it like OOP

--- app/controllers/tasks/task_completions_controller.rb
+++ app/controllers/tasks/task_completions_controller.rb
@@ -3,18 +3,14 @@ module Tasks
     # POST /tasks/:task_id/completion
     def create
       task = Task.find(params[:task_id])
-      complete_task(task)
+
+      if task.complete
+        flash.notice = "Task was successfully completed."
+      else
+        flash.alert = "Something went wrong"
+      end
 
       redirect_to tasks_path
-    end
-
-    private
-
-    def complete_task(task)
-      TaskCompletion.create_or_find_by!(task_id: task.id)
-      flash.notice = "Task was successfully completed."
-    rescue
-      flash.alert = "Something went wrong"
     end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,13 @@ class Task < ApplicationRecord
 
   has_one :task_completion
 
+  def complete
+    TaskCompletion.create_or_find_by!(task_id: id)
+    true
+  rescue
+    false
+  end
+
   def completed_at
     task_completion&.created_at
   end
